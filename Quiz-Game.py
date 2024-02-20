81ed0c04347ba1d20dc62e0ebd084f16c3d4bd92
Mahtabvariyani <mahtabvariyani@gmail.com>
2024-02-20 11:38:47 +0100

    Quiz

--- Quiz-Game.py
+++ Quiz-Game.py
@@ -1,1 +1,50 @@
-print('It Works')
+print("Welcome to My Quiz")
+
+playing = input("Do you want to play a game? ").lower()
+if playing != "yes":
+    quit()
+
+print("Okay... Let's Play...!!!")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+    score -= 1
+
+answer = input("What is the capital of France? ")
+if answer.lower() == "paris":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+    score -= 1
+
+answer = input("What is the largest planet in our solar system? ")
+if answer.lower() == "jupiter":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+    score -= 1
+
+answer = input("Who wrote 'Romeo and Juliet'? ")
+if answer.lower() == "william shakespeare":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+    score -= 1
+
+answer = input("What is the chemical symbol for water? ")
+if answer.lower() == "h2o":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+    score -= 1
+
+print("Your final Percentage is:", (score / 4) * 100, "%")
