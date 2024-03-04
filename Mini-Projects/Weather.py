9ec2991ccb78ab0183942d7d61cc259edf12c8bd
Mahtabvariyani <mahtabvariyani@gmail.com>
2024-03-04 12:50:08 +0100

    Weather app

--- Mini-Projects/Weather.py
+++ Mini-Projects/Weather.py
@@ -1,1 +1,22 @@
-#https://www.youtube.com/watch?v=baWzHKfrvqw
+import requests
+
+
+# https://www.youtube.com/watch?v=baWzHKfrvqw
+# print("This is a Weather API App")
+# API Key d6d0f72fc1a27093423f960188c5bb11
+
+
+api_key = "d6d0f72fc1a27093423f960188c5bb11"
+
+user_input = input("Write name of a  City: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}"
+)
+
+
+weather = weather_data.json()['weather'][0]['main']
+temp = weather_data.json()['main']['temp']
+
+
+print(weather,temp)
